3072266d76147653a0b9666fe5fb76e125392f1a
AHMED ABDULWAHAB <s338486830@ocdsb>
2018-03-29 09:39:56 -0400

    almost done, I think I need to improve how it looks and some stuff for question 4.

--- main.py
+++ main.py
@@ -15,41 +15,45 @@ def FtoC (tempF):
     return 'It would be: ~' + str(round(tempC)) + ' degrees Celsius.'
 
 
-print("[]-------------------------------------------[]")
+
 while True:
-    choice = str(input("Press C to convert from Fahrenheit to Celsius\nPress F to convert from Celsius to Fahrenheit\n"))
+    print()
+    print("[]-------------------------------------------[]")
+    choice = str(input("Press [C] to convert from Fahrenheit to Celsius\nPress [F] to convert from Celsius to Fahrenheit\n>>> "))
     #lets user pick between the 2 functions.
 
     if choice == str("F") or choice == str("f"): #added an or statement so it isn't case sensitive   
-        temperature = int(input('Enter your temperature in Celsius: '))
+        temperature = int(input('Enter your temperature in Celsius:\n>>> '))
         
         if temperature < -273.15:
             print("Not possible, absolute zero is -273 in Celsius.")
-            if int(input("Enter 1 to try another number ")) == 1:
+            if int(input("Enter 1 to try another conversion\n>>> ")) == 1:
                 continue
             else:
-                break
-            
+                break  
         else:
             print(CtoF(temperature))
-            break
-
-
-    elif choice == str("C") or choice == str("c"):
-       temperature = int(input('Enter your temperature in Fahrenheit: '))
-       
-       if temperature < -459.67:
-            print("Not possible, absolute zero is -460 in Fahrenheit.")
-            if int(input("Enter 1 to try another number ")) == 1:
+            if int(input("Enter 1 to try another conversion\n>>> ")) == 1:
                 continue
             else:
                 break
-            
-       else:
-            print(FtoC(temperature))
-            break
+              
+    elif choice == str("C") or choice == str("c"):
+           while True:   
+                  temperature = int(input('Enter your temperature in Fahrenheit:\n>>> '))
+                  if temperature < -459.67:
+                         print("Not possible, absolute zero is -460 in Fahrenheit.")
+                         continue
+                  else:
+                    print(FtoC(temperature))
+                    print()
+                    if int(input("Enter 1 to try another Fahrenheit conversion\nPress 2 to do a different conversion\n>>> ")) == 1:
+                            continue
+                    elif input() == 2:
+                            break
+                    
             
     else:
-        print(":thinking emoji:")
-        quit()
+        print("why")
+        continue
     #passive aggresive comment if they don't pick an actual choice 
