53915b07d93325f6585b3f31d17dea1f5f800609
Ahmed Abdulwahab <aabdu76@ocdsb.ca>
2018-03-28 21:52:29 -0400

    who knows

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # <Ahmed Abdulwahab>
 # March 28, 2018
 
+
 #celc to farh function.
 def CtoF (tempC):
     tempF = (1.8 * tempC) + 32 
@@ -15,26 +16,40 @@ def FtoC (tempF):
 
 
 print("[]-------------------------------------------[]")
-choice = str(input("Press C to convert from Fahrenheit to Celsius\nPress F to convert from Celsius to Fahrenheit\n"))
-#lets user pick between the 2 functions.
+while True:
+    choice = str(input("Press C to convert from Fahrenheit to Celsius\nPress F to convert from Celsius to Fahrenheit\n"))
+    #lets user pick between the 2 functions.
 
-if choice == str("F") or choice == str("f"): #added an or statement so it isn't case sensitive   
-    temperature = int(input('Enter your temperature in Celsius: '))
-    if temperature < -273.15:
-        print("Not possible, absolute zero is -273 in Celsius.")
-        quit()
+    if choice == str("F") or choice == str("f"): #added an or statement so it isn't case sensitive   
+        temperature = int(input('Enter your temperature in Celsius: '))
+        
+        if temperature < -273.15:
+            print("Not possible, absolute zero is -273 in Celsius.")
+            if int(input("Enter 1 to try another number ")) == 1:
+                continue
+            else:
+                break
+            
+        else:
+            print(CtoF(temperature))
+            break
+
+
+    elif choice == str("C") or choice == str("c"):
+       temperature = int(input('Enter your temperature in Fahrenheit: '))
+       
+       if temperature < -459.67:
+            print("Not possible, absolute zero is -460 in Fahrenheit.")
+            if int(input("Enter 1 to try another number ")) == 1:
+                continue
+            else:
+                break
+            
+       else:
+            print(FtoC(temperature))
+            break
+            
     else:
-        print(CtoF(temperature))
-
-elif choice == str("C") or choice == str("c"):
-    temperature = int(input('Enter your temperature in Fahrenheit: '))
-    if temperature < -459.67:
-        print("Not possible, absolute zero is -460 in Fahrenheit.")
+        print(":thinking emoji:")
         quit()
-    else:
-        print(FtoC(temperature))
-
-else:
-    print(":thinking emoji:")
-    quit()
-#passive aggresive comment if they don't pick an actual choice 
+    #passive aggresive comment if they don't pick an actual choice 
